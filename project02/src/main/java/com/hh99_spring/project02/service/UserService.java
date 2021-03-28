975058309788b864d3bc5254a34f3c8053a14c77
[strong1133] <[strong1133@naver.com]>
2021-03-29 02:34:55 +0900

    [validate]

--- project02/src/main/java/com/hh99_spring/project02/service/UserService.java
+++ project02/src/main/java/com/hh99_spring/project02/service/UserService.java
@@ -3,6 +3,7 @@ package com.hh99_spring.project02.service;
 import com.hh99_spring.project02.domain.User;
 import com.hh99_spring.project02.dto.SignupRequestDto;
 import com.hh99_spring.project02.repository.UserRepository;
+import com.hh99_spring.project02.util.SignupValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,8 +22,12 @@ public class UserService {
     public void registerUser(SignupRequestDto signupRequestDto) {
         //ID
         String username = signupRequestDto.getUsername();
+        //정규식 검사
+        if(!SignupValidator.idValid(username)){
+            throw new IllegalArgumentException("idValid");
+        }
+        //중복검사
         Optional<User> found = userRepository.findByUsername(username);
-
         if (found.isPresent()) {
             throw new IllegalArgumentException("duplicate");
         }
@@ -30,12 +35,22 @@ public class UserService {
         //PW
         String password = signupRequestDto.getPassword();
         String password_check = signupRequestDto.getPassword_check();
-
+        //정규식 검사
+        if(!SignupValidator.pwValid(username, password)){
+            throw new IllegalArgumentException("pwValid");
+        }
+        //비밀번호 재입력 일치 검사
         if (!password.equals(password_check)) {
             throw new IllegalArgumentException("notequal");
         }
+        //모든조건 충족시 비밀번호 암호화
         password = passwordEncoder.encode(signupRequestDto.getPassword());
+
+        //Email
         String email = signupRequestDto.getEmail();
+        if (!SignupValidator.emailValid(email)){
+            throw new IllegalArgumentException("emailValid");
+        }
 
         User user = new User(username, password, email);
         userRepository.save(user);
--- project02/src/main/resources/static/js/login.js
+++ project02/src/main/resources/static/js/login.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 function blank_check(){
 
     frm1 = document.signup_form;
-    if(frm1.username.value==''){
+    if(frm1.username.value=='' || frm1.password.value=='' || frm1.password_check.value=='' || frm1.email.value=='' ){
         alert('필수 입력란이 비었습니다! 확인해주세요.')
         return false;
     }
